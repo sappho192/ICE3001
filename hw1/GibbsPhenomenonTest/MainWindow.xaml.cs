f04ab3b0420034237d974581fa432cd95919cdf1
sappho192 <sappho192@gmail.com>
2019-11-13 22:47:04 +0900

    불연속점은 오차계산에서 생략, RMSE 계산 추가

--- hw1/GibbsPhenomenonTest/MainWindow.xaml.cs
+++ hw1/GibbsPhenomenonTest/MainWindow.xaml.cs
@@ -43,7 +43,23 @@ namespace GibbsPhenomenonTest
 
             InitializeApproxTab();
             InitializeErrorTab();
+            UpdateErrors();
+        }
+
+        private void UpdateErrors()
+        {
             UpdateAbsErr();
+            UpdateRMSErr();
+        }
+
+        private void UpdateRMSErr()
+        {
+            double RMSE =
+                Math.Sqrt(
+                    eN_t.Sum((e) => Math.Pow(e, 2))
+                    / eN_t.Length
+                );
+            tbRMSError.Text = RMSE.ToString();
         }
 
         private void UpdateAbsErr()
@@ -61,8 +77,7 @@ namespace GibbsPhenomenonTest
 
         private void InitializeErrorTab()
         {
-            eN_t = x_t.Zip(xN_t, (a, b) => a - b).ToArray();
-            eNGraph.Plot(dis_t, eN_t);
+            RefreshErrorGraphs();
         }
 
         private void InitializeApproxTab()
@@ -95,10 +110,16 @@ namespace GibbsPhenomenonTest
 
         private void BtApply_Click(object sender, RoutedEventArgs e)
         {
-            if(uint.TryParse(tbN.Text, out uint result) && result != 0)
+            ApplyChanges();
+        }
+
+        private void ApplyChanges()
+        {
+            if (uint.TryParse(tbN.Text, out uint result) && result != 0)
             {
                 test.N = result;
                 RefreshGraphs();
+                UpdateErrors();
             }
             else
             {
@@ -115,6 +136,14 @@ namespace GibbsPhenomenonTest
         private void RefreshErrorGraphs()
         {
             eN_t = x_t.Zip(xN_t, (a, b) => a - b).ToArray();
+
+            // distance: t 축에서 1만큼의 거리가 delta_t 간격으로 얼마나 나뉘었는지
+            uint distance = (uint)(1.0 / test.Delta_t);
+            for (uint i = 0; i < eN_t.Length; i += distance)
+            {// 시작점과 끝점이 정수이므로 불연속점을 알 수 있음
+                eN_t[i] = 0; // 불연속점에서의 오차는 생략 처리함
+            }
+
             eNGraph.Plot(dis_t, eN_t);
         }
 
@@ -125,6 +154,14 @@ namespace GibbsPhenomenonTest
             xN_t = Approx.SetOf(test.xN_t, FROM, TO, test.Delta_t, test.N);
             graph_x_t.Plot(dis_t, x_t);
             graph_xN_t.Plot(dis_t, xN_t);
+        }
+
+        private void TbN_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                ApplyChanges();
+            }
         }
     }
 }
